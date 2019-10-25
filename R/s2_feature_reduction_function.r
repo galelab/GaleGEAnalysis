c44892c9511a38b1e1b959452d01f21df32142bc
Leanne <leanne382@gmail.com>
2019-10-25 12:12:57 -0700

    have code s2_feature_reduction_function.r working with pca from RNAseqDE libray probably going change over to stats package next iteration

---
+++ R/s2_feature_reduction_function.r
@@ -0,0 +1,85 @@
+#' s2 feature reduction 
+#'
+#' This function does feature reduction and visualization on normalized voom counts
+#' @param countfile raw counts table (generally output by htseq).
+#' @param targetfile target file.
+#' @param  figres resolution at which to output figures (default is 300).
+#' @keywords gene expression feature reduction
+#' @export
+#' @import limma
+#' @import edgeR
+#' @import Biobase
+#' @import ExpressionNormalizationWorkflow
+#' @import stringr
+#' @examples
+#' s2_feature_reduction(count_file.txt, target_file.csv, vizualize_data=TRUE, FilterGenesWithCounts=100)
+
+s2_feature_reduction <- function(countfile, targetfile, figres=100) { 
+    # V.CPM <- readRDS('./s1_norm_raw_counts_results/1.voomobject.rds')
+    pdf(NULL)
+    files <- loadfiles(count_file=countfile, target_file=targetfile)
+    results_path <- generate_folder('s2_feature_reduction_results')
+    vizualize_feature_reduction_data(files$counts, files$target$treatment,results_path, figres)
+    # pvca(files$counts, files$target, results_path)
+    pca(files$counts, files$target, results_path)
+
+}
+
+vizualize_feature_reduction_data <- function(data, labels, results_path, figres=100) { 
+    ###MDS (multidimensional scaling) uses log fold changes between genes as distances
+    MDS <- plotMDS(data, gene.selection='pairwise', cex= .8)
+    minx<-min(MDS$x)
+    maxx<-max(MDS$x)
+    miny<-min(MDS$y)
+    maxy<-max(MDS$y)
+    png(file.path(results_path,  '2.mds_vnorm_matrix.png'), res = figres)
+    plot(MDS$x, MDS$y, cex=1, xlim=c(minx-1, maxx+1), ylim=c(miny-1, maxy+1), xlab=paste0(MDS$axislabel,' 1'), ylab=paste0(MDS$axislabel,' 2'), frame = FALSE)
+    text(MDS$x, MDS$y, labels, cex=0.6, pos=4)
+    dev.off()
+}
+
+pvca <- function(exprs, covrts, results_path, figres=100) {
+    #Principal Component analysis of variation (PVCA)
+
+    inpData <- expSetobj(exprs, covrts)
+    cvrts_eff_var <- colnames(covrts) ## Set the covariates whose effect size on the data needs to be calculated
+    cvrts_eff_var_f <- c()
+    for (val in cvrts_eff_var) {
+        val1 <- lapply(val, tolower)
+        test <- grep('id', val1)
+        print (val1)
+        print(length(test))
+        print(test)
+        if (length(test) == 1) { 
+        } else { 
+            cvrts_eff_var_f <- append(cvrts_eff_var_f, val, after = length(cvrts_eff_var_f))
+        }
+    }
+    # cvrts_eff_var <- cvrts_eff_var[-1]
+    pct_thrsh <- 0.75
+    png(file.path(results_path,  '2.pvca_vnorm_matrix.png'), res = figres)
+    pvcAnaly(inpData, pct_thrsh, cvrts_eff_var_f)
+    dev.off()
+}
+
+
+pca<-function(exprs, labels, results_path, figres=100) {
+    normcounts <-exprs
+    normcounts300<-normcounts[sample(1:nrow(normcounts),300),]
+    pca12 <-RNAseqDE::counts2PCA(counts = normcounts300, info = labels$Animal_Outcome, ids = targets$animal.ID)
+    vizualize_pca(file.path(results_path, '2.pca_vnorm_matrix.png'), pca12, labels$Sex, labels$Animal_Outcome, figres)
+
+}
+
+vizualize_pca<-function(plot_file, PCA, class1, class2, figres) { 
+    minx<-min(PCA[[1]]$PC1)
+    maxx<-max(PCA[[1]]$PC1)
+    miny<-min(PCA[[1]]$PC2)
+    maxy<-max(PCA[[1]]$PC2)
+    png(plot_file, res = figres)
+    plot(PCA[[1]]$PC1, PCA[[1]]$PC2, frame=FALSE, ylim=c(miny-5, maxy+5), xlim=c(minx-5, maxx+5), pch=as.numeric(as.factor(class1)), col=as.numeric(as.factor(class2)) )
+
+    legend("topright", bty = "n", pch=as.numeric(as.factor(as.numeric(as.factor(class1)))),  legend= levels(as.factor(class1)))
+    legend("bottomright", bty = "n", pch='-', col=levels(as.factor(as.numeric(as.factor(class2)))), legend= c(levels(as.factor(class2))))
+    dev.off()
+}
