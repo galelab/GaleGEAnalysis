4ca5adfade7ceadd175961ed87052dbbe4b84aea
Leanne <leanne382@gmail.com>
2019-11-05 12:57:15 -0800

    fix bugs in R/s3_DE_analysis.r specifically if statements

--- R/s3_DE_analysis.r
+++ R/s3_DE_analysis.r
@@ -37,26 +37,40 @@ s3_DE_analysis <- function(countfile, targetfile, target_class=10, blocking_colu
     if (exists("design") == FALSE | exists("eset_voom") == FALSE) { 
         print ('WARNING: could not find design and voom model... make sure user is in the correct working directory')
     } else { 
-        print (exists("blocking_column"))
-        if (blocking_column != FALSE) {
+        print (' 1')
+        if (typeof(blocking_column) != 'logical') {
             BLOCKID  <- factor(files$targets[,blocking_column], levels=unique(files$targets[,blocking_column]))
-        }
-        if (blocking_column != FALSE) {
-            if (corfit != FALSE) {
+            print (' 2')
+            if (exists('corfit') == TRUE) {
                 fit <- lmFit(eset_voom,design,block=BLOCKID, correlation=corfit$consensus)
             } else { 
                 fit <- lmFit(eset_voom,design,block=BLOCKID)
             }
         } else { 
-            if (corfit != FALSE) {
+            print (' 3')
+            if (exists('corfit') == TRUE) {
                 fit <- lmFit(eset_voom,design, correlation=corfit$consensus)
             } else { 
                 fit <- lmFit(eset_voom,design)
             }
         }
-        if (matrixfile != FALSE) {
+        if (typeof(matrixfile) == 'character') {
+            print (' 4')
             matrix_contrast <- scan(matrixfile,  character(), quote = "")
-            cont.matrix <- makeContrasts(matrix, levels=design)
+            print (matrix_contrast)
+            print (' 5')
+            # cont.matrix <- makeContrasts(contrasts=matrix_contrast, levels=design)
+            cont.matrix <- makeContrasts(
+            (treatmentInfected_BL - treatmentUninfected_BL),
+            (treatmentInfected_w1 - treatmentUninfected_w1),
+            (treatmentInfected_w18 - treatmentUninfected_w18),
+            (treatmentInfected_w19 - treatmentUninfected_w19),
+            (treatmentInfected_w25 - treatmentUninfected_w25),
+            (treatmentInfected_w26 - treatmentUninfected_w26),
+            (treatmentInfected_w27 - treatmentUninfected_w27),
+            (treatmentInfected_w41 - treatmentUninfected_w41),
+            levels=design)
+            print (' 6')
             fit <- contrasts.fit(fit, cont.matrix)
             fit <-eBayes(fit)
             results <- decideTests(fit, lfc=log2(log_fold_change), method="separate", adjust.method="BH", p.value=p_value)
