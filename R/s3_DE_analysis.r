46bac69b326aa5d1c05e69eaefee0d2af3063dc4
Leanne <leanne382@gmail.com>
2019-11-05 15:14:48 -0800

    update s3 DE analysis code to pull out significantly differently expressed genes

--- R/s3_DE_analysis.r
+++ R/s3_DE_analysis.r
@@ -8,6 +8,7 @@
 #' @export
 #' @import edgeR
 #' @import limma
+#' @import gplots 
 #' @examples
 #' 
 
@@ -37,17 +38,15 @@ s3_DE_analysis <- function(countfile, targetfile, target_class=10, blocking_colu
     if (exists("design") == FALSE | exists("eset_voom") == FALSE) { 
         print ('WARNING: could not find design and voom model... make sure user is in the correct working directory')
     } else { 
-        print (' 1')
         if (typeof(blocking_column) != 'logical') {
             BLOCKID  <- factor(files$targets[,blocking_column], levels=unique(files$targets[,blocking_column]))
-            print (' 2')
+
             if (exists('corfit') == TRUE) {
                 fit <- lmFit(eset_voom,design,block=BLOCKID, correlation=corfit$consensus)
             } else { 
                 fit <- lmFit(eset_voom,design,block=BLOCKID)
             }
         } else { 
-            print (' 3')
             if (exists('corfit') == TRUE) {
                 fit <- lmFit(eset_voom,design, correlation=corfit$consensus)
             } else { 
@@ -55,28 +54,48 @@ s3_DE_analysis <- function(countfile, targetfile, target_class=10, blocking_colu
             }
         }
         if (typeof(matrixfile) == 'character') {
-            print (' 4')
             matrix_contrast <- scan(matrixfile,  character(), quote = "")
-            print (matrix_contrast)
-            print (' 5')
-            # cont.matrix <- makeContrasts(contrasts=matrix_contrast, levels=design)
-            cont.matrix <- makeContrasts(
-            (treatmentInfected_BL - treatmentUninfected_BL),
-            (treatmentInfected_w1 - treatmentUninfected_w1),
-            (treatmentInfected_w18 - treatmentUninfected_w18),
-            (treatmentInfected_w19 - treatmentUninfected_w19),
-            (treatmentInfected_w25 - treatmentUninfected_w25),
-            (treatmentInfected_w26 - treatmentUninfected_w26),
-            (treatmentInfected_w27 - treatmentUninfected_w27),
-            (treatmentInfected_w41 - treatmentUninfected_w41),
-            levels=design)
-            print (' 6')
+
+            cont.matrix <- makeContrasts(contrasts=matrix_contrast, levels=design)
+
             fit <- contrasts.fit(fit, cont.matrix)
             fit <-eBayes(fit)
+
             results <- decideTests(fit, lfc=log2(log_fold_change), method="separate", adjust.method="BH", p.value=p_value)
-            summary(results)
+            print (summary(results))
             a <- vennCounts(results)
-            print(a)
+            # print(a)
+            write.fit(fit, file="SG_Watkins_DE.txt", digits=3, method="separate", adjust="BH")
+            DE_HGNC <- read.csv("SG_Watkins_DE.txt", header = T,row.names = 1, check.names=FALSE,sep = "\t")
+            rhesus2human <- read.csv(file="rhesus2human.csv", header=TRUE, stringsAsFactors = FALSE)
+            print (' 1')
+            DE_HGNC <- merge(rhesus2human, DE_HGNC, by.x='Gene.stable.ID', by.y='row.names')
+            print (' 2')
+            DE_HGNC <- avereps(DE_HGNC, ID = DE_HGNC$HGNC.symbol)
+            # print (' 3')
+            # print (DE_HGNC)
+            # row.names(DE_HGNC) <- unique(DE_HGNC[,1])
+            # print (' 4')
+            # write.csv(DE_HGNC, file="DEgenes_HGNC.csv")
+            dataMatrix <- fit$coefficients # Extract results of differential expression
+
+            sigMask <- dataMatrix * (results**2) # 1 if significant, 0 otherwise
+
+            ExpressMatrix <- subset(dataMatrix, rowSums(sigMask) != 0) # filter for significant genes
+
+            # Filter sigMask to use for selecting DE genes from ExpressMatrix
+            sigMask <- subset(sigMask, rowSums(sigMask) != 0)
+
+            dim(sigMask)
+
+            length(sigMask)
+
+            write.csv(ExpressMatrix, file="ExpressMatrix_separate_Watkins.csv")
+            # source('heatmap2.F.R')
+            png("Watkins_heatmap_djn.png",width = 8, height = 10, units = 'in', res = 300)
+            global_modules <- heatmap.F.4(ExpressMatrix, cutoff = 1, distmethod = "euclidean", clustermethod = "ward.D", clusterdim='row')
+            dev.off()
+ 
         } else { 
             print ('WARNING: need to specify matrix file')
         }
