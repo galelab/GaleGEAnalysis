84c26dc7de9f7aca01a6dfb668f6b013f4ae21c6
Leanne <leanne382@gmail.com>
2019-11-13 13:13:53 -0800

    clean up code   R/s4_gene_enrichment_analysis_function.r

--- R/s4_gene_enrichment_analysis_function.r
+++ R/s4_gene_enrichment_analysis_function.r
@@ -11,36 +11,36 @@
 #' 
 
 s4_gene_enrichment_analysis<-function(DEgenes='./s3_DE_results/3.ExpressMatrix_separate_LFC_HGNC_AV.csv', log_values_column=7, pvalue=0.1, NumTopGoTerms=10) {
-    results_path <- generate_folder('s4_gene_enrichment_results')
-    results_path <- generate_folder(paste0('s4_gene_enrichment_results/column',log_values_column))
+    results_path  <- generate_folder('s4_gene_enrichment_results')
+    results_path  <- generate_folder(paste0('s4_gene_enrichment_results/column',log_values_column))
     unlink(paste0(results_path,'/*'))
 
-    genefile <- read.csv(DEgenes)
+    genefile      <- read.csv(DEgenes)
     
-    genernk <- genefile[,c(3,log_values_column)]
+    genernk       <- genefile[,c(3,log_values_column)]
 
     write.table(genernk, sep='\t',quote = FALSE, col.names=FALSE, row.names=FALSE, file=file.path(results_path,'DEgenes.rnk'))
 
-    genes <- read.table(file.path(results_path,'DEgenes.rnk'), sep='\t')
-    geneList <- genes[,2]
+    genes         <- read.table(file.path(results_path,'DEgenes.rnk'), sep='\t')
+    geneList      <- genes[,2]
     names(geneList) = as.character(genes[,1])
-    x<-names(geneList)
+    x             <-names(geneList)
 
     topGOdata     <- new("topGOdata", description="topGO BP", ontology="BP",nodeSize=5, allGenes=geneList, geneSel=function(p) p>pvalue, annot=annFUN.org, mapping="org.Hs.eg.db", ID="SYMBOL")
     resultFisher  <- runTest(topGOdata, algorithm="classic", statistic="fisher")
-    resultKS <- runTest(topGOdata, algorithm ="classic", statistic='ks')
+    resultKS      <- runTest(topGOdata, algorithm ="classic", statistic='ks')
     allRes        <- GenTable(topGOdata, classicFisher=resultFisher, classicKS=resultKS, orderBy ="classicFisher", topNodes=NumTopGoTerms)
     write.table(allRes, file=file.path(results_path, 'GOTermTable.csv'),sep=',', row.names=FALSE)
-    goIDs          <- allRes$GO.ID
+    goIDs         <- allRes$GO.ID
     
     for (gt in goIDs) {
         print (paste0('STATUS: getting genes in ', gt, ' term'))
         term.genes <- genesInTerm(topGOdata, gt)
-        affID <- term.genes[[gt]]
-        pval <- sort(geneScore(topGOdata, affID, use.names = TRUE))
+        affID      <- term.genes[[gt]]
+        pval       <- sort(geneScore(topGOdata, affID, use.names = TRUE))
         # pval <- pval[pval <= pvalue]
-        affID <- names(pval)
-        tablex<-select(org.Hs.eg.db, affID, c("GENENAME"), "ALIAS")
+        affID      <- names(pval)
+        tablex     <-select(org.Hs.eg.db, affID, c("GENENAME"), "ALIAS")
         for (i in 1:length(pval)) {
             tablex[['logFC']][i]<-pval[i]
         }
