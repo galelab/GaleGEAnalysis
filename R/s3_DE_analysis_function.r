76b50c284d70f234604989e36280b114082ec2a6
Leanne <leanne382@gmail.com>
2019-12-31 13:28:24 -0800

    test for slack

--- R/s3_DE_analysis_function.r
+++ R/s3_DE_analysis_function.r
@@ -20,7 +20,7 @@
  
 
 s3_DE_analysis <- function(countfile='./s1_norm_raw_counts_results/1.norm_matrix.txt', targetfile='./p1_modified_count_matrix_results/target_file.csv',  gene_conversion_file=FALSE, blocking_column=FALSE, matrixfile=FALSE, pvalue=0.05, logfoldchange=1.5) { 
-
+    print ('test')
     results_path <- generate_folder('s3_DE_results')
     unlink('./s3_DE_results/*')
 
