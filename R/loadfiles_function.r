222402c48f3633b8727be07dedeb8eaa17b6844a
Leanne <leanne382@gmail.com>
2020-01-23 11:43:48 -0800

    add sort function to   R/loadfiles_function.r which sorts input target and count files

--- R/loadfiles_function.r
+++ R/loadfiles_function.r
@@ -9,7 +9,11 @@
 loadfiles <- function(count_file, target_file) {
     #Load in count data  and target information from csv'
     counts    <- read.table(count_file, header = TRUE, sep = "\t", row.names=1, as.is = TRUE, check.names = FALSE)
+    counts    <- counts[, order(names(counts))]
+
     targets   <- read.table(target_file, header = TRUE, sep = ",", row.names=1, as.is = TRUE, check.names = FALSE)
+    targets   <- targets[order(rownames(targets)), ]
+
     results   <- list("counts" = counts, "targets" = targets)
     return(results)
 }
