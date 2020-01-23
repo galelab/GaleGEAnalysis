7691ccd747eb9290e006f341dc44ff34b458029f
Leanne <leanne382@gmail.com>
2020-01-23 09:55:21 -0800

    fix dumb bug in  R/p1_modify_count_matrix_function.r which was not outputing column names correctly in the ./p1 count_mod.txt file.  Also add capability to sort both columns and rows in outputed count and target files respectively

--- R/p1_modify_count_matrix_function.r
+++ R/p1_modify_count_matrix_function.r
@@ -13,25 +13,30 @@ p1_modify_count_matrix <- function(countfile, targetfile, samples_to_remove_coun
     #remove samples to not include analyses
     if (typeof(samples_to_remove_count_matrix) == 'character') {
         if ((grep('.txt', samples_to_remove_count_matrix) == 1) || (grep('.csv', samples_to_remove_count_matrix) == 1)) {
-            samples_to_remove <- read.table(samples_to_remove_count_matrix, header=FALSE, row.names=1)
+            samples_to_remove              <- read.table(samples_to_remove_count_matrix, header=FALSE, row.names=1, check.names = FALSE)
             samples_to_remove_count_matrix <- rownames(samples_to_remove)
         }
         if (length(samples_to_remove_count_matrix) > 0) {
 
             files <- loadfiles(countfile, targetfile)
-            # print (dim(files$counts))
-            # print (dim(files$targets))
 
+            #Remove unecessaary samples from count matrix
             modcounts <- files$counts[ , !(names(files$counts) %in% samples_to_remove_count_matrix)]
+            #Sort column names in count matrix 
+            modcounts <- modcounts[, order(names(modcounts))]
+
+            #Remove unecessaary samples from target file
             modtargets <- files$targets[!(rownames(files$targets) %in% samples_to_remove_count_matrix), ]
-            # print(dim(modcounts))
-            # print(dim(modtargets))
+            ##Sorts targets by row names so hopefully it matches ordered column rows above in count matrix
+            modtargets <- modtargets[order(rownames(modtargets)), ]
+
 
             print(paste0('STATUS: The number of samples removed were ', length(samples_to_remove_count_matrix)))
             results_path <- generate_folder('p1_modified_count_matrix_results')
 
-            write.table(data.frame("Name"=rownames(modcounts), modcounts), sep = "\t", row.names=FALSE, file=file.path(results_path,"count_matrix_mod.txt"))
-            write.csv(data.frame("Name"=rownames(modtargets), modtargets), row.names=FALSE, file=file.path(results_path,"targets_mod.csv"))
+            write.table(data.frame("Name"=rownames(modcounts), modcounts, check.names=FALSE), sep = "\t",col.names=TRUE,  row.names=FALSE, file=file.path(results_path,"count_matrix_mod.txt"))
+
+            write.csv(data.frame("Name"=rownames(modtargets), modtargets, check.names=FALSE), row.names=FALSE, file=file.path(results_path,"targets_mod.csv"))
             
             results <- list("counts" = modcounts, "targets" = modtargets)   
         }
