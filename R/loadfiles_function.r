d77bd40c07b1d88981bbb884b901e9beed1f8d20
Leanne <leanne382@gmail.com>
2020-04-30 10:02:39 -0700

    make it possible to only load count or target files  in R/loadfiles_function.r

--- R/loadfiles_function.r
+++ R/loadfiles_function.r
@@ -8,15 +8,23 @@
 
 loadfiles <- function(count_file, target_file) {
     #Load in count data  and target information from csv'
-    counts    <- read.table(count_file, header = TRUE,
+    if (typeof(count_file) == "character") {
+        counts    <- read.table(count_file, header = TRUE,
                             sep = "\t", row.names = 1,
                             as.is = TRUE, check.names = FALSE)
-    counts    <- counts[, order(names(counts))]
+        counts    <- counts[, order(names(counts))]
+    } else if ((is.FALSE(count_file)) || (is.null(count_file))) {
+        counts <- FALSE
+    }
 
-    targets   <- read.table(target_file, header = TRUE,
-                            sep = ",", row.names = 1,
-                            as.is = TRUE, check.names = FALSE)
-    targets   <- targets[order(rownames(targets)), ]
+    if (typeof(target_file) == "character") {
+        targets   <- read.table(target_file, header = TRUE,
+                                sep = ",", row.names = 1,
+                                as.is = TRUE, check.names = FALSE)
+        targets   <- targets[order(rownames(targets)), ]
+    } else if (((is.FALSE(target_file)) || is.null(count_file))) {
+        targets <- FALSE
+    }
 
     results   <- list("counts" = counts, "targets" = targets)
     return(results)
